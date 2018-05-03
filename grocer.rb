b28fff96c244eaad5eba39d2b149c70eeb1452b4
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-03 03:06:34 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,27 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
-end
+  
+  cart.each do |cart_item|
+    cart_item.each do |item, details|
+      cart_item.each_with_object(Hash.new(0)) { |item,details| counts[item] += 1 }
+    
+    binding.pry
+    
+    end #end cart_item.each
+  end #end cart.each
+    
+  
+  # cart.each do |cart_item|
+  #     cart_item.each do |item, details|
+  #         details.each do |key, value|
+            
+  #   binding.pry
+  #         end #end details each 
+  #     end #end cart_item.each
+  # end #end cart.each
+  
+end #end consolidate method
 
 def apply_coupons(cart, coupons)
   # code here
